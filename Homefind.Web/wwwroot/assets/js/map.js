d72166ca8ae9acce0ff25d0a5a66349544db5a80
Silviu Vermesan <svermesan@endava.com>
2019-01-31 10:37:03 +0200

    Details map place markers changes

--- Homefind.Web/wwwroot/assets/js/map.js
+++ Homefind.Web/wwwroot/assets/js/map.js
@@ -12,7 +12,8 @@ jQuery(document).ready(function ($) {
 
     //google map custom marker icon - .png fallback for IE11
     var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-    var $marker_url = (is_internetExplorer11) ? '/assets/img/location.png' : '/assets/img/location.png';
+    var $marker_red = (is_internetExplorer11) ? '/assets/img/marker-red.png' : '/assets/img/marker-red.png';
+    var $marker_blue = (is_internetExplorer11) ? '/assets/img/marker-blue.png' : '/assets/img/marker-blue.png';
 
     //define the basic color of your map, plus a value for saturation and brightness
     var $main_color = '#2d313f',
@@ -193,8 +194,7 @@ jQuery(document).ready(function ($) {
     function getMapOptions(geoposition) {
         //set google map options
         var map_options = {
-            center: geoposition,
-            zoom: 16,
+            zoom: 15,
             panControl: false,
             zoomControl: false,
             mapTypeControl: false,
@@ -215,8 +215,8 @@ jQuery(document).ready(function ($) {
         success: function (address) {
             geocoder.geocode({ 'address': address }, function (results, status) {
                 if (status === 'OK') {
-                    currentGeoposition = results[0].geometry.location;
-                    SetMapMarkerDetails(results[0], 14);
+                    currentGeoposition = results[0];
+                    SetMapMarkerDetails(results[0], 15);
                 } else {
                     alert('Geocode was not successful for the following reason: ' + status);
                 }
@@ -244,34 +244,51 @@ jQuery(document).ready(function ($) {
         findNearbyPlaces('supermarket');
     });
 
+    $(".fa-car").click(function () {
+        setTrafficLayer();
+    });
+
+    function setTrafficLayer() {
+        var map = new google.maps.Map(document.getElementById('map-container'), getMapOptions(currentGeoposition));
+
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+
+        createMarker(currentGeoposition, map, $marker_red);
+        map.setCenter(currentGeoposition.geometry.location);
+    }
+
     function findNearbyPlaces(place) {
-        map = new google.maps.Map(document.getElementById('conatiner-map'), getMapOptions(currentGeoposition));
+        map = new google.maps.Map(document.getElementById('map-container'), getMapOptions(currentGeoposition));
 
         var request = {
-            location: currentGeoposition,
+            location: currentGeoposition.geometry.location,
             radius: '500',
             type: ['' + place + '']
         };
 
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch(request, nearbyCallback);
+
+        map.setCenter(currentGeoposition.geometry.location);
     }
 
     function nearbyCallback(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
-                createMarker(place, map);
+                createMarker(place, map, $marker_blue);
             }
         }
+        createMarker(currentGeoposition, map, $marker_red);
     }
 
-    var createMarker = function (place, map) {
+    var createMarker = function (place, map, icon) {
         var marker = new google.maps.Marker({
             position: place.geometry.location,
             map: map,
             visible: true,
-            icon: $marker_url
+            icon: icon
         });
 
         var infowindow = new google.maps.InfoWindow();
@@ -284,7 +301,6 @@ jQuery(document).ready(function ($) {
     var SetMapMarkerDetails = function (place, map_zoom) {
         //set google map options
         var map_options = {
-            center: place.geometry.location,
             zoom: map_zoom,
             panControl: false,
             zoomControl: false,
@@ -295,9 +311,9 @@ jQuery(document).ready(function ($) {
             styles: style
         };
         //inizialize the map
-        var map = new google.maps.Map(document.getElementById('conatiner-map'), map_options);
+        var map = new google.maps.Map(document.getElementById('map-container'), map_options);
         //add a custom marker to the map				
-        createMarker(place, map);
+        createMarker(place, map, $marker_red);
 
         //add custom buttons for the zoom-in/zoom-out on the map
         function CustomZoomControl(controlDiv, map) {
@@ -312,5 +328,6 @@ jQuery(document).ready(function ($) {
 
         //insert the zoom div on the top left of the map
         map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+        map.setCenter(currentGeoposition.geometry.location);
     };
 });
